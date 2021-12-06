b8718f8d020248e50f19bd97b05d7ee0cd65d1bd
Stephen Early <steve@assorted.org.uk>
2021-12-06 09:23:26 +0000

    Day 6: now with a list instead of a dict

--- day06.py
+++ day06.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
 
 with open("day06-input.txt") as f:
-    fish = { n: 0 for n in range(9) }
+    fish = [ 0 ] * 9
     for f in (int(x) for x in f.read().split(',')):
         fish[f] += 1
 
 def gen():
-    global fish
-    fish = {
-        8: fish[0],
-        7: fish[8],
-        6: fish[7] + fish[0],
-        5: fish[6],
-        4: fish[5],
-        3: fish[4],
-        2: fish[3],
-        1: fish[2],
-        0: fish[1],
-    }
+    fish.append(r := fish.pop(0))
+    fish[6] += r
 
 for _ in range(80):
     gen()
 
-print(f"Part 1: {sum(fish.values())}")
+print(f"Part 1: {sum(fish)}")
 
 for _ in range(80, 256):
     gen()
 
-print(f"Part 2: {sum(fish.values())}")
+print(f"Part 2: {sum(fish)}")
