093488adb54aa2ec5924045a7e41ba0a5e3a0495
Stephen Early <steve@assorted.org.uk>
2021-12-14 10:30:48 +0000

    Running count of elements is simpler

--- day14.py
+++ day14.py
@@ -9,15 +9,13 @@ with open("day14-input.txt") as f:
 
 def run(count):
     p = Counter(zip(template, template[1:]))
+    c = Counter(template)
     for _ in range(count):
         p, prev_p = Counter(), p
         for pair, count in prev_p.items():
             p[(pair[0], rules[pair])] += count
             p[(rules[pair], pair[1])] += count
-    c = Counter()
-    for pair, count in p.items():
-        c[pair[0]] += count
-    c[template[-1]] += 1
+            c[rules[pair]] += count
     return max(c.values()) - min(c.values())
 
 print(f"Part 1: {run(10)}")
